a1eeeade0a5fadaa11077f372f9a58e71620d288
Ratnesh Chauhan <ratneshchauhan279@gmail.com>
2025-05-03 20:20:30 +0530

    Add files via upload

---
+++ 1007(Minimum Domino Rotations For Equal Row).py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def minDominoRotations(self, tops, bottoms):
+        """
+        :type tops: List[int]
+        :type bottoms: List[int]
+        :rtype: int
+        """
+        def check(x):
+            rotations_top = rotations_bottom = 0
+            for i in range(len(tops)):
+                if tops[i] != x and bottoms[i] != x:
+                    return -1
+                elif tops[i] != x:
+                    rotations_top += 1
+                elif bottoms[i] != x:
+                    rotations_bottom += 1
+            return min(rotations_top, rotations_bottom)
+
+        result = check(tops[0])
+        if result != -1 or tops[0] == bottoms[0]:
+            return result
+        return check(bottoms[0])
