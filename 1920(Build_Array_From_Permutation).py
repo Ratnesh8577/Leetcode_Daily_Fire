8db2c60004c5b116805529e08bb7a5e96a620ea8
Ratnesh Chauhan <ratneshchauhan279@gmail.com>
2025-05-06 20:16:42 +0530

    Add files via upload

---
+++ 1920(Build_Array_From_Permutation).py
@@ -0,0 +1,20 @@
+"""
+class Solution(object):
+    def buildArray(self, nums):
+        n = len(nums)
+        for i in range(n):
+            nums[i] += (nums[nums[i]] % n) * n
+        for i in range(n):
+            nums[i] //= n
+        return nums
+
+"""
+
+class Solution(object):
+    def buildArray(self, nums):
+        n = len(nums)
+        ans=[0]*n
+        for i in range(0,n):
+            ans[i]=nums[nums[i]]
+        return ans
+
