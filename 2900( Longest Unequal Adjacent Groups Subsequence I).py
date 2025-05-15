ff2ca48fe1c3413121b2c5560bad407c99392a93
Ratnesh Chauhan <ratneshchauhan279@gmail.com>
2025-05-15 14:52:50 +0530

    Add files via upload

---
+++ 2900( Longest Unequal Adjacent Groups Subsequence I).py
@@ -0,0 +1,14 @@
+class Solution:
+    def getLongestSubsequence(self, words, groups):
+        if not words:
+            return []
+
+        result = [words[0]]
+        last_group = groups[0]
+
+        for i in range(1, len(words)):
+            if groups[i] != last_group:
+                result.append(words[i])
+                last_group = groups[i]
+
+        return result
