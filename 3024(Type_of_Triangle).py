2f3e8b65256f460ae8aaedf71985171400714694
Ratnesh Chauhan <ratneshchauhan279@gmail.com>
2025-05-19 18:17:46 +0530

    Add files via upload

---
+++ 3024(Type_of_Triangle).py
@@ -0,0 +1,15 @@
+class Solution:
+    def triangleType(self, nums: list[int]) -> str:
+        a,b,c=nums
+        if a+b<=c or b+c<=a or a+c<=b:
+            return "none"
+        
+        elif a==b==c:
+            return "equilateral"
+        elif a==b or b==c or a==c:
+            return "isosceles"
+        else:
+            return "scalene"
+        
+
+        
